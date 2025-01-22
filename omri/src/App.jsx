71dd152743d0f04145e2409b1fa693d2c722bc2e
daniperdomo <111296277+daniperdomo@users.noreply.github.com>
2025-01-21 22:59:39 -0400

    Implementacion del router

--- omri/src/App.jsx
+++ omri/src/App.jsx
@@ -1,24 +1,35 @@
-import Nav from "./components/Nav"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Accesorios from "./pages/Accesorios";
+import Cubitt from "./pages/cubitt";
+import Nosotros from "./pages/nosotros";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* navbar */}
-      <div className="bg-black">
-        <Nav />
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        {/* navbar */}
+        <div className="bg-black">
+          <Nav />
+        </div>
+
+        {/* Main content (grow to fill available space) */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cubitt' element={<Cubitt />} />
+            <Route path='/accesorios' element={<Accesorios />} />
+            <Route path='/nosotros' element={<Nosotros />} />
+          </Routes>
+        </main>
+
+        {/* footer */}
+        <Footer />
       </div>
-
-      {/* Main content (grow to fill available space) */}
-      <main className="flex-grow">
-        <Home />
-      </main>
-
-      {/* footer */}
-      <Footer />
-    </div>
-  )
+    </Router>
+  );
 }
 
 export default App;
--- omri/src/components/Nav.jsx
+++ omri/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-scroll';
 import { FaTimes, FaBars } from 'react-icons/fa'; 
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     const [click, setClick] = useState(false);
@@ -9,26 +9,30 @@ const Nav = () => {
     return (
         <nav>
             <div className="h-20 flex justify-between items-center z-50 text-white lg:py-5 px-10 sm:px-6">
-                {/*contenedor del logo */}
+                {/* Contenedor del logo */}
                 <div className="flex items-center flex-1">
-                    {/*recuerda ajustar altura y ancho a conveniencia */}
-                    <img src="/images/logofondonegro.png" alt="Logo" className="h-20 w-auto md:h-20 lg:h-32 cursor-pointer"/>
+                    <img src="/images/logofondonegro.png" alt="Logo" className="h-20 w-auto md:h-20 lg:h-32 cursor-pointer" />
                 </div>
-                
+
                 <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
                     <div className="flex-10">
                         <ul className="flex gap-8 mr-16 text-lg">
-                            <Link spy={true} smooth={true} to="Home">
-                            <li className="hover:text-color-hover transition border-b-2 border-black hover:border-color-hover cursor-pointer">
+                            <Link to="/">
+                                <li className="hover:text-color-hover transition border-b-2 border-black hover:border-color-hover cursor-pointer">
                                     Inicio
                                 </li>
                             </Link>
-                            <Link spy={true} smooth={true} to="Productos">
-                            <li className="hover:text-color-hover transition border-b-2 border-black hover:border-color-hover cursor-pointer">
-                                    Productos
+                            <Link to="/cubitt">
+                                <li className="hover:text-color-hover transition border-b-2 border-black hover:border-color-hover cursor-pointer">
+                                    Cubitt
                                 </li>
                             </Link>
-                            <Link spy={true} smooth={true} to="Nosotros">
+                            <Link to="/accesorios">
+                                <li className="hover:text-color-hover transition border-b-2 border-black hover:border-color-hover cursor-pointer">
+                                    Accesorios
+                                </li>
+                            </Link>
+                            <Link to="/nosotros">
                                 <li className="hover:text-color-hover transition border-b-2 border-black hover:border-color-hover cursor-pointer">
                                     Nosotros
                                 </li>
